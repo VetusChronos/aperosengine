0e54b0de2905c0e53b1dfa43b9d4c045718cbbd2
Chronos <emanueljunior756@gmail.com>
2024-08-02 22:01:58 -0300

    refactor(light): C++17 features and changes to code formatting

--- src/light.cpp
+++ src/light.cpp
@@ -18,8 +18,9 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #include "light.h"
-#include <algorithm>
+
 #include <cmath>
+#include <numeric>
 #include "util/numeric.h"
 #include "settings.h"
 
@@ -30,7 +31,6 @@ static u8 light_LUT[LIGHT_SUN + 1];
 // The const ref to light_LUT is what is actually used in the code
 const u8 *light_decode_table = light_LUT;
 
-
 struct LightingParams {
 	float a, b, c; // Lighting curve polynomial coefficients
 	float boost, center, sigma; // Lighting curve parametric boost
@@ -39,53 +39,43 @@ struct LightingParams {
 
 static LightingParams params;
 
-
-float decode_light_f(float x)
-{
-	if (x >= 1.0f) // x is often 1.0f
-		return 1.0f;
-	x = std::fmax(x, 0.0f);
+float decode_light_f(float x) {
+	if (x >= 1.0f) return 1.0f;
+	x = std::max(x, 0.0f);
 	float brightness = ((params.a * x + params.b) * x + params.c) * x;
-	brightness += params.boost *
-		std::exp(-0.5f * sqr((x - params.center) / params.sigma));
-	if (brightness <= 0.0f) // May happen if parameters are extreme
-		return 0.0f;
-	if (brightness >= 1.0f)
-		return 1.0f;
+	brightness += params.boost * std::exp(-0.5f * sqr((x - params.center) / params.sigma));
+	brightness = std::clamp(brightness, 0.0f, 1.0f);
+
 	return powf(brightness, 1.0f / params.gamma);
 }
 
-
 // Initialize or update the light value tables using the specified gamma
-void set_light_table(float gamma)
-{
-// Lighting curve bounding gradients
-	const float alpha = rangelim(g_settings->getFloat("lighting_alpha"), 0.0f, 3.0f);
-	const float beta  = rangelim(g_settings->getFloat("lighting_beta"), 0.0f, 3.0f);
-// Lighting curve polynomial coefficients
+void set_light_table(float gamma) {
+	// Lighting curve bounding gradients
+	const float alpha = std::clamp(g_settings->getFloat("lighting_alpha"), 0.0f, 3.0f);
+	const float beta  = std::clamp(g_settings->getFloat("lighting_beta"), 0.0f, 3.0f);
+	// Lighting curve polynomial coefficients
 	params.a = alpha + beta - 2.0f;
 	params.b = 3.0f - 2.0f * alpha - beta;
 	params.c = alpha;
-// Lighting curve parametric boost
-	params.boost = rangelim(g_settings->getFloat("lighting_boost"), 0.0f, 0.4f);
-	params.center = rangelim(g_settings->getFloat("lighting_boost_center"), 0.0f, 1.0f);
-	params.sigma = rangelim(g_settings->getFloat("lighting_boost_spread"), 0.0f, 0.4f);
-// Lighting curve gamma correction
-	params.gamma = rangelim(gamma, 0.33f, 3.0f);
+	// Lighting curve parametric boost
+	params.boost = std::clamp(g_settings->getFloat("lighting_boost"), 0.0f, 0.4f);
+	params.center = std::clamp(g_settings->getFloat("lighting_boost_center"), 0.0f, 1.0f);
+	params.sigma = std::clamp(g_settings->getFloat("lighting_boost_spread"), 0.0f, 0.4f);
+	// Lighting curve gamma correction
+	params.gamma = std::clamp(gamma, 0.33f, 3.0f);
 
-// Boundary values should be fixed
+	// Boundary values should be fixed
 	light_LUT[0] = 0;
 	light_LUT[LIGHT_SUN] = 255;
 
 	for (size_t i = 1; i < LIGHT_SUN; i++) {
-		float brightness = decode_light_f((float)i / LIGHT_SUN);
-		// Strictly speaking, rangelim is not necessary here—if the implementation
-		// is conforming. But we don’t want problems in any case.
-		light_LUT[i] = rangelim((s32)(255.0f * brightness), 0, 255);
+		float brightness = decode_light_f(static_cast<float>(i) / LIGHT_SUN);
+		light_LUT[i] = std::clamp(static_cast<s32>(255.0f * brightness), 0, 255);
 
 		// Ensure light brightens with each level
-		if (i > 0 && light_LUT[i] <= light_LUT[i - 1]) {
-			light_LUT[i] = std::min((u8)254, light_LUT[i - 1]) + 1;
+		if (light_LUT[i] <= light_LUT[i - 1]) {
+			light_LUT[i] = std::min(static_cast<u8>(254), light_LUT[i - 1]) + 1;
 		}
 	}
 }
--- src/light.h
+++ src/light.h
@@ -18,22 +18,23 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #pragma once
+
 #include <cassert>
+#include <algorithm>
+
 #include "irrlichttypes.h"
 
-/*
-	Lower level lighting stuff
-*/
+/* Lower level lighting stuff */
 
 // This directly sets the range of light.
 // Actually this is not the real maximum, and this is not the brightest, the
 // brightest is LIGHT_SUN.
 // If changed, this constant as defined in builtin/game/constants.lua must
 // also be changed.
-#define LIGHT_MAX 14
+constexpr int LIGHT_MAX = 14;
 // Light is stored as 4 bits, thus 15 is the maximum.
 // This brightness is reserved for sunlight
-#define LIGHT_SUN 15
+constexpr int LIGHT_SUN = 15;
 
 #ifndef SERVER
 
@@ -47,17 +48,13 @@ with this program; if not, write to the Free Software Foundation, Inc.,
  *
  * The array is a lookup table to convert the internal representation of light
  * (brightness) to the display brightness.
- *
  */
 extern const u8 *light_decode_table;
 
 // 0 <= light <= LIGHT_SUN
 // 0 <= return value <= 255
-inline u8 decode_light(u8 light)
-{
-	// assert(light <= LIGHT_SUN);
-	if (light > LIGHT_SUN)
-		light = LIGHT_SUN;
+inline u8 decode_light(u8 light) {
+	assert(light <= LIGHT_SUN);
 	return light_decode_table[light];
 }
 
@@ -72,11 +69,8 @@ void set_light_table(float gamma);
 // 0 <= daylight_factor <= 1000
 // 0 <= lightday, lightnight <= LIGHT_SUN
 // 0 <= return value <= LIGHT_SUN
-inline u8 blend_light(u32 daylight_factor, u8 lightday, u8 lightnight)
-{
+inline u8 blend_light(u32 daylight_factor, u8 lightday, u8 lightnight) {
 	u32 c = 1000;
 	u32 l = ((daylight_factor * lightday + (c - daylight_factor) * lightnight)) / c;
-	if (l > LIGHT_SUN)
-		l = LIGHT_SUN;
-	return l;
+	return static_cast<u8>(std::min(l, static_cast<u32>(LIGHT_SUN)));
 }
