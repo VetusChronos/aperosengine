01c4dcef688a867703ae3f0c9f31e64aed90e780
Chronos <emanueljunior756@gmail.com>
2024-08-19 18:25:40 -0300

    refactor(daynightratio.h): updating the code to C++17

--- src/daynightratio.h
+++ src/daynightratio.h
@@ -18,54 +18,55 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 */
 
 #pragma once
+#include <array>
 
 inline u32 time_to_daynight_ratio(float time_of_day, bool smooth) {
-	float t = time_of_day;
-	if (t < 0.0f)
-		t += ((int)(-t) / 24000) * 24000.0f;
-	if (t >= 24000.0f)
-		t -= ((int)(t) / 24000) * 24000.0f;
-	if (t > 12000.0f)
-		t = 24000.0f - t;
+	// Normalize time_of_day to be within [0, 24000)
+	time_of_day = fmod(time_of_day + 24000.0f, 24000.0f);
+	if (time_of_day > 12000.0f) {
+		time_of_day = 24000.0f - time_of_day;
+	}
 
-	const float values[9][2] = {
-		{ 4250.0f + 125.0f, 175.0f },
-		{ 4500.0f + 125.0f, 175.0f },
-		{ 4750.0f + 125.0f, 250.0f },
-		{ 5000.0f + 125.0f, 350.0f },
-		{ 5250.0f + 125.0f, 500.0f },
-		{ 5500.0f + 125.0f, 675.0f },
-		{ 5750.0f + 125.0f, 875.0f },
-		{ 6000.0f + 125.0f, 1000.0f },
-		{ 6250.0f + 125.0f, 1000.0f },
-	};
+	constexpr std::array<std::array<float, 2>, 9> values = {{
+		{4375.0f, 175.0f},
+		{4625.0f, 175.0f},
+		{4875.0f, 250.0f},
+		{5125.0f, 350.0f},
+		{5375.0f, 500.0f},
+		{5625.0f, 675.0f},
+		{5875.0f, 875.0f},
+		{6125.0f, 1000.0f},
+		{6375.0f, 1000.0f},
+	}};
 
 	if (!smooth) {
 		float lastt = values[0][0];
-		for (u32 i = 1; i < 9; i++) {
+		for (size_t i = 1; i < values.size(); i++) {
 			float t0 = values[i][0];
 			float switch_t = (t0 + lastt) / 2.0f;
 			lastt = t0;
-			if (switch_t <= t)
-				continue;
+			if (switch_t <= time_of_day) continue;
 
 			return values[i][1];
 		}
+
 		return 1000;
 	}
 
-	if (t <= 4625.0f) // 4500 + 125
+	if (time_of_day <= 4625.0f) { // 4500 + 125
 		return values[0][1];
-	else if (t >= 6125.0f) // 6000 + 125
+	} else if (time_of_day >= 6125.0f) { // 6000 + 125
 		return 1000;
-
-	for (u32 i = 0; i < 9; i++) {
-		if (values[i][0] <= t)
-			continue;
-
-		float td0 = values[i][0] - values[i - 1][0];
-		float f = (t - values[i - 1][0]) / td0;
-		return f * values[i][1] + (1.0f - f) * values[i - 1][1];
 	}
+
+	// Smooth interpolation for time values between known points
+	for (size_t i = 1; i < values.size(); i++) {
+		if (values[i][0] > time_of_day) {
+			float td0 = values[i][0] - values[i - 1][0];
+			float f = (time_of_day - values[i - 1][0]) / td0;
+			return static_cast<u32>(f * values[i][1] + (1.0f - f) * values[i - 1][1]);
+		}
+	}
+
 	return 1000;
 }
